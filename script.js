8d1681221892c088226f665f7d76f08c22183e0d
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-11-02 10:40:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+
+let a = 30;
+
+if (a > 10) {
+
+    console.log(a + 100)
+
+} else {
+    console.log(a - 30)
+
+}
+
+let as = 7;
+if (as) {
+    console.log(7 / 2)
+} else {
+    console.log(7 * 3)
+}
+
+
+
+let b = 30;
+if (b < 50) {
+    console.log(" то выведите квадрат этого числа,");
+
+}
+else (b > 10 && b < 30); {
+
+    console.log("то выведите ноль");
+
+}
+
+
+
+
+let d = 7;
+let ds = 6;
+if (d > ds) {
+    console.log(d);
+} else if (ds < d) {
+    console.log(ds);
+}
+
+
+
+let y = 23;
+let yr = 78;
+let yrt = (y, yr);
+if (yrt = 100) {
+    console.log(' Да');
+} else {
+    console.log(no);
+}
+
+
+
+
+
+
+let month;
+if (month == 24 || month == 12 || month == 7) {
+    console.log(winter)
+} else if (month == 1 || month == 30 || month == 24) {
+    console.log(february)
+} else if (month == 8 || month == 3 || month == 24) {
+    console.log(March)
+} else {
+    console.log("Ошибка");
+}
+
+
+
+
+
+
+let wes = 27;
+let wer = 30;
+let wea = 22;
+if (wes > wer && wes > wea) {
+    console.log(wes);
+} else if (wer > wes && wer > wea) {
+    console.log(wer);
+} else if (wea > wes && wea > wer) {
+    console.log(wea);
+}
